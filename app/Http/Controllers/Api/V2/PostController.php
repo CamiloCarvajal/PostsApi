a4e10a501ba4469291551d2ff3cc15a5d17eeecc
Camilo Carvajal <camilocc597@gmail.com>
2022-12-20 02:08:56 +0000

    config routes

--- app/Http/Controllers/Api/V2/PostController.php
+++ app/Http/Controllers/Api/V2/PostController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
+use App\Http\Resources\V2\PostResource;
+use App\Http\Resources\V2\PostCollection;
+
+
 class PostController extends Controller
 {
     /**
